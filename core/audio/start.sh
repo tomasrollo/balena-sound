93a4cd265188b7af5ea6982e70415c11fd0e7aae
Tomas Rollo <tomas.rollo@toyota-europe.com>
2021-01-26 21:37:29 +0100

    Removed unnecessary elements

--- core/audio/start.sh
+++ core/audio/start.sh
@@ -13,40 +13,11 @@ function set_loopback_latency() {
   sed -i "s/%$LOOPBACK%/$LATENCY/" "$CONFIG_FILE"
 }
 
-# Route "balena-sound.input" to the appropriate sink depending on selected mode
+# Route "balena-sound.input" to the appropriate sink
 # Either "snapcast" fifo sink or "balena-sound.output"
 function route_input_sink() {
-  local MODE="$1"
-
-  declare -A options=(
-      ["MULTI_ROOM"]=0
-      ["MULTI_ROOM_CLIENT"]=1
-      ["STANDALONE"]=2
-    )
-
-  case "${options[$MODE]}" in
-    ${options["STANDALONE"]} | ${options["MULTI_ROOM_CLIENT"]})
-      sed -i "s/%INPUT_SINK%/sink='balena-sound.output'/" "$CONFIG_FILE"
-      echo "Routing 'balena-sound.input' to 'balena-sound.output'."
-      ;;
-
-    ${options["MULTI_ROOM"]} | *)
-      sed -i "s/%INPUT_SINK%/sink=\"snapcast\"/" "$CONFIG_FILE"
-      echo "Routing 'balena-sound.input' to 'snapcast'."
-      ;;
-  esac
-}
-
-# Route any existing source to the input router ("balena-sound.input")
-function route_input_source() {
-  local INPUT_DEVICE=$(arecord -l | awk '/card [0-9]:/ { print $3 }')
-
-  if [[ -n "$INPUT_DEVICE" ]]; then
-    local INPUT_DEVICE_FULLNAME="alsa_input.$INPUT_DEVICE.analog-stereo"
-    echo "Routing audio from '$INPUT_DEVICE_FULLNAME' into 'balena-sound.input sink'"
-    echo -e "\nload-module module-loopback source='$INPUT_DEVICE_FULLNAME' sink='balena-sound.input'" >> "$CONFIG_FILE"
-  fi
-
+  sed -i "s/%INPUT_SINK%/sink='balena-sound.output'/" "$CONFIG_FILE"
+  echo "Routing 'balena-sound.input' to 'balena-sound.output'."
 }
 
 # Route "balena-sound.output" to the appropriate audio hardware
@@ -71,15 +42,6 @@ function reset_sound_config() {
   cp "$CONFIG_TEMPLATE" "$CONFIG_FILE"
 }
 
-# Wait for sound supervisor to start
-SOUND_SUPERVISOR="$(ip route | awk '/default / { print $3 }'):3000"
-while ! curl --silent --output /dev/null "$SOUND_SUPERVISOR/ping"; do sleep 5; echo "Waiting for sound supervisor to start"; done
-
-# Get mode from sound supervisor. 
-# mode: default to MULTI_ROOM
-SOUND_SUPERVISOR="$(ip route | awk '/default / { print $3 }'):3000"
-MODE=$(curl --silent "$SOUND_SUPERVISOR/mode" || true)
-
 # Get latency values
 SOUND_INPUT_LATENCY=${SOUND_INPUT_LATENCY:-200}
 SOUND_OUPUT_LATENCY=${SOUND_OUTPUT_LATENCY:-200}
@@ -87,12 +49,9 @@ SOUND_OUPUT_LATENCY=${SOUND_OUTPUT_LATENCY:-200}
 # Audio routing: route intermediate balena-sound input/output sinks
 echo "Setting audio routing rules. Note that this can be changed after startup."
 reset_sound_config
-route_input_sink "$MODE"
+route_input_sink
 route_output_sink
 set_loopback_latency "INPUT_LATENCY" "$SOUND_INPUT_LATENCY"
 set_loopback_latency "OUTPUT_LATENCY" "$SOUND_OUPUT_LATENCY"
-if [[ -n "$SOUND_ENABLE_SOUNDCARD_INPUT" ]]; then
-  route_input_source
-fi
 
 exec pulseaudio --file /etc/pulse/balena-sound.pa
